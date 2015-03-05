c35836114e0a81b56f9c8ffd7e6e4ac10f16b22c
wellscorp <welton.s92@gmail.com>
2015-03-05 15:51:36 -0300

    cadastros

--- js/index.js
+++ js/index.js
@@ -16,6 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var database= null;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -35,7 +37,11 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        alert("pronto!");
+        //alert("pronto!");
+        var db= window.openDatabase("banco", "1.0", "banco de dados", 200000);
+        //database= createOpen("teste", "1.0", "testando selecao", 200000);
+        db.transaction(PopulateDatabase, errorDB, successDB);
+        //alert(db);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -52,69 +58,31 @@ var app = {
 };
 
 
-// --------------------------------- GRAFICOS ---------------------------------------
-
-$('#container').highcharts({
-    title: {
-        text: 'Monthly Average Temperature',
-        x: -20 //center
-    },
-    subtitle: {
-        text: 'Source: WorldClimate.com',
-        x: -20
-    },
-    xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    yAxis: {
-        title: {
-            text: 'Temperature (°C)'
-        },
-        plotLines: [{
-            value: 0,
-            width: 1,
-            color: '#808080'
-        }]
-    },
-    tooltip: {
-        valueSuffix: '°C'
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle',
-        borderWidth: 0
-    },
-    series: [{
-        name: 'Tokyo',
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }, {
-        name: 'New York',
-        data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-    }, {
-        name: 'Berlin',
-        data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-    }, {
-        name: 'London',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-    }]
-});
-
 
 
 // ----------------------- CAPTURAR FOTO ---------------------
 
 
-function capturePhoto(){
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail,{
-        destinationType:destinationType.DATA_URL
-    });
+
+
+
+function PopulateDatabase(tx){
+
+    tx.executeSql("Create TABLE IF NOT EXISTS chamado (ROLLNO INT, titulo text, descricao text)");
+    //tx.executeSql("Insert into chamado values(1,'titulo 01', 'descricao 01')");
+
+}
+
+function errorDB(error){
+    alert("Erro ao inserir no banco: " + error);
+}
+
+function successDB(){
+    alert("Informações inseridas com sucesso!");
 }
 
 
-
-
+/*
 db = window.sqlitePlugin.openDatabase({name: "DB"});
 db.transaction(function(tx) {
     // Cria a Tabela "tabela_testes"
@@ -130,3 +98,4 @@ db.transaction(function(tx) {
         }
     });
 });
+*/
